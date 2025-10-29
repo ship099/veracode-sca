b2f560eb7b7d27b43af5646b12bb4dc79848d005
Shipra Kumari <skumari@veracode.com>
2025-10-29 15:48:29 +0530

    Powerhsell command

--- src/srcclr.ts
+++ src/srcclr.ts
@@ -47,199 +47,203 @@ export async function runAction (options: Options)  {
         core.info(command);
         const jsonCommand = `curl -sSL https://download.sourceclear.com/ci.sh | sh -s -- scan ${extraCommands} --json=${SCA_OUTPUT_FILE}`;
 
-        if (options.createIssues) {
-            core.info('Starting the scan')
-          const execution = spawn('sh',['-c',command],{
-            stdio:"pipe",
-            shell:false
-          });
+        const windowsCommand = `iex (Invoke-RestMethod -Uri 'https://example.com/ci.ps1') -s -- scan ${extraCommands} ${commandOutput}`
+        const output = execSync(windowsCommand, { encoding: 'utf-8', maxBuffer: 1024 * 1024 * 10 });//10MB
+        console.log(output);
+        // if (options.createIssues) {
+        //     core.info('Starting the scan')
+        //   const execution = spawn('sh',['-c',command],{
+        //     stdio:"pipe",
+        //     shell:false
+        //   });
           
-          execution.on('error', (data) => {
-              core.error(data);
-          })
+        //   execution.on('error', (data) => {
+        //       core.error(data);
+        //   })
                 
-          let output: string = '';
-          execution.stdout!.on('data', (data) => {
-              output = `${output}${data}`;
-          });
+        //   let output: string = '';
+        //   execution.stdout!.on('data', (data) => {
+        //       output = `${output}${data}`;
+        //   });
             
-          execution.stderr!.on('data', (data) => {
-              core.error(`stderr: ${data}`);
-          });
+        //   execution.stderr!.on('data', (data) => {
+        //       core.error(`stderr: ${data}`);
+        //   });
 
-          execution.on('close', async (code) => {
-            core.info('Create issue "true" - on close')
-            if (core.isDebug()){
-                core.info(output);
-            }
+        //   execution.on('close', async (code) => {
+        //     core.info('Create issue "true" - on close')
+        //     if (core.isDebug()){
+        //         core.info(output);
+        //     }
 
-            //Pull request decoration
-            core.info('check if we run on a pull request')
-            let pullRequest = process.env.GITHUB_REF
-            let isPR:any = pullRequest?.indexOf("pull")
-            let summary_message = ""
+        //     //Pull request decoration
+        //     core.info('check if we run on a pull request')
+        //     let pullRequest = process.env.GITHUB_REF
+        //     let isPR:any = pullRequest?.indexOf("pull")
+        //     let summary_message = ""
 
-            if ( isPR >= 1 ){
-                core.info('We run on a PR, add more messaging')
-                const context = github.context
-                const repository:any = process.env.GITHUB_REPOSITORY
-                const repo = repository.split("/");
-                const commentID:any = context.payload.pull_request?.number
-                let pr_header = '<br>![](https://www.veracode.com/themes/veracode_new/library/img/veracode-black-hires.svg)<br>'
-                summary_message = `Veracode SCA Scan finished with exit code: ${code}. Please review created and linked issues`
+        //     if ( isPR >= 1 ){
+        //         core.info('We run on a PR, add more messaging')
+        //         const context = github.context
+        //         const repository:any = process.env.GITHUB_REPOSITORY
+        //         const repo = repository.split("/");
+        //         const commentID:any = context.payload.pull_request?.number
+        //         let pr_header = '<br>![](https://www.veracode.com/themes/veracode_new/library/img/veracode-black-hires.svg)<br>'
+        //         summary_message = `Veracode SCA Scan finished with exit code: ${code}. Please review created and linked issues`
 
-                try {
-                    const octokit = github.getOctokit(options.github_token);
+        //         try {
+        //             const octokit = github.getOctokit(options.github_token);
         
-                    const { data: comment } = await octokit.rest.issues.createComment({
-                        owner: repo[0],
-                        repo: repo[1],
-                        issue_number: commentID,
-                        body: pr_header+summary_message,
-                    });
-                    core.info('Adding scan results message as comment to PR #'+commentID)
-                } catch (error:any) {
-                    core.info(error);
-                }
-            }
-            else {
-                summary_message = `Veracode SCA Scan finished with exit code: ${code}. Please review created issues`
-            }
+        //             const { data: comment } = await octokit.rest.issues.createComment({
+        //                 owner: repo[0],
+        //                 repo: repo[1],
+        //                 issue_number: commentID,
+        //                 body: pr_header+summary_message,
+        //             });
+        //             core.info('Adding scan results message as comment to PR #'+commentID)
+        //         } catch (error:any) {
+        //             core.info(error);
+        //         }
+        //     }
+        //     else {
+        //         summary_message = `Veracode SCA Scan finished with exit code: ${code}. Please review created issues`
+        //     }
 
-            //Generate issues
-            run(options,core.info);
+        //     //Generate issues
+        //     run(options,core.info);
 
-            core.info(summary_message);
-            // if scan was set to fail the pipeline should fail and show a summary of the scan results
-            if ( code != null && code > 0 ){
-                core.setFailed(summary_message)
-            }
+        //     core.info(summary_message);
+        //     // if scan was set to fail the pipeline should fail and show a summary of the scan results
+        //     if ( code != null && code > 0 ){
+        //         core.setFailed(summary_message)
+        //     }
 
-            //store output files as artifacts
-            core.info('Store json Results as Artifact')
-            const { DefaultArtifactClient } = require('@actions/artifact');
-            const artifactClient = new DefaultArtifactClient();
-            const artifactName = 'Veracode Agent Based SCA Results';
-            const files = [
-                'scaResults.json'
-            ]
+        //     //store output files as artifacts
+        //     core.info('Store json Results as Artifact')
+        //     const { DefaultArtifactClient } = require('@actions/artifact');
+        //     const artifactClient = new DefaultArtifactClient();
+        //     const artifactName = 'Veracode Agent Based SCA Results';
+        //     const files = [
+        //         'scaResults.json'
+        //     ]
             
-            const rootDirectory = process.cwd()
-            const artefactOptions = {
-                continueOnError: true
-            }
+        //     const rootDirectory = process.cwd()
+        //     const artefactOptions = {
+        //         continueOnError: true
+        //     }
             
-            const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, artefactOptions)
+        //     const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, artefactOptions)
 
 
 
 
-            core.info('Finish command');
-         });
+        //     core.info('Finish command');
+        //  });
 
         
-        } else {
-            core.info('Command to run: '+command)
-            const execution = spawn('sh',['-c',command],{
-                stdio:"pipe",
-                shell:false
-              });
+        // } else {
+        //     core.info('Command to run: '+command)
+        //     const execution = spawn('sh',['-c',command],{
+        //         stdio:"pipe",
+        //         shell:false
+        //       });
 
-            execution.on('error', (data) => {
-                core.error(data);
-            })
+        //     execution.on('error', (data) => {
+        //         core.error(data);
+        //     })
                     
-            let output: string = '';
-            let jsonOutput: string = '';
-            execution.stdout!.on('data', (data) => {
-                output = `${output}${data}`;
-            });
+        //     let output: string = '';
+        //     let jsonOutput: string = '';
+        //     execution.stdout!.on('data', (data) => {
+        //         output = `${output}${data}`;
+        //     });
                 
-            execution.stderr!.on('data', (data) => {
-                core.error(`stderr: ${data}`);
-            });
+        //     execution.stderr!.on('data', (data) => {
+        //         core.error(`stderr: ${data}`);
+        //     });
     
-            execution.on('close', async (code) => {
-                //core.info(output);
-                core.info(`Scan finished with exit code:  ${code}`);
+        //     execution.on('close', async (code) => {
+        //         //core.info(output);
+        //         core.info(`Scan finished with exit code:  ${code}`);
 
-                core.info(output)
+        //         core.info(output)
              
 
-                try {
-                    writeFileSync('scaResults.txt', output);
-                    console.log('The file has been saved!');
-                } catch (err) {
-                    console.error('Error writing file:', err);
-                }
+        //         try {
+        //             writeFileSync('scaResults.txt', output);
+        //             console.log('The file has been saved!');
+        //         } catch (err) {
+        //             console.error('Error writing file:', err);
+        //         }
 
 
-                //store output files as artifacts
-                core.info('Store txt Results as Artifact')
-                const { DefaultArtifactClient } = require('@actions/artifact');
-                const artifactClient = new DefaultArtifactClient();
-                const artifactName = 'Veracode Agent Based SCA Results';
-                const files = [
-                    'scaResults.txt'
-                ]
+        //         //store output files as artifacts
+        //         core.info('Store txt Results as Artifact')
+        //         const { DefaultArtifactClient } = require('@actions/artifact');
+        //         const artifactClient = new DefaultArtifactClient();
+        //         const artifactName = 'Veracode Agent Based SCA Results';
+        //         const files = [
+        //             'scaResults.txt'
+        //         ]
                 
-                const rootDirectory = process.cwd()
-                const artefactOptions = {
-                    continueOnError: true
-                }
+        //         const rootDirectory = process.cwd()
+        //         const artefactOptions = {
+        //             continueOnError: true
+        //         }
                 
-                const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, artefactOptions)
+        //         const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, artefactOptions)
             
-                //Pull request decoration
-                core.info('check if we run on a pull request')
-                let pullRequest = process.env.GITHUB_REF
-                let isPR:any = pullRequest?.indexOf("pull")
+        //         //Pull request decoration
+        //         core.info('check if we run on a pull request')
+        //         let pullRequest = process.env.GITHUB_REF
+        //         let isPR:any = pullRequest?.indexOf("pull")
 
-                if ( isPR >= 1 ){
-                    core.info("This run is part of a PR, should add some PR comment")
+        //         if ( isPR >= 1 ){
+        //             core.info("This run is part of a PR, should add some PR comment")
             
-                    const context = github.context
-                    const repository:any = process.env.GITHUB_REPOSITORY
-                    const repo = repository.split("/");
-                    const commentID:any = context.payload.pull_request?.number
+        //             const context = github.context
+        //             const repository:any = process.env.GITHUB_REPOSITORY
+        //             const repo = repository.split("/");
+        //             const commentID:any = context.payload.pull_request?.number
 
 
-                    let commentBody = '<br>![](https://www.veracode.com/sites/default/files/2022-04/logo_1.svg)<br>'
-                    commentBody += "<pre>Veraocde SCA Scan finished with exit code "+code+"\n"
-                    commentBody += '\n<details><summary>Veracode SCA Scan details</summary><p>\n'
-                    commentBody += output //.replace(/    /g, '&nbsp;&nbsp;&nbsp;&nbsp;');
-                    commentBody += '</p></details>\n</pre>'
+        //             let commentBody = '<br>![](https://www.veracode.com/sites/default/files/2022-04/logo_1.svg)<br>'
+        //             commentBody += "<pre>Veraocde SCA Scan finished with exit code "+code+"\n"
+        //             commentBody += '\n<details><summary>Veracode SCA Scan details</summary><p>\n'
+        //             commentBody += output //.replace(/    /g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+        //             commentBody += '</p></details>\n</pre>'
 
 
                 
 
-                    try {
-                        const octokit = github.getOctokit(options.github_token);
+        //             try {
+        //                 const octokit = github.getOctokit(options.github_token);
             
-                        const { data: comment } = await octokit.rest.issues.createComment({
-                            owner: repo[0],
-                            repo: repo[1],
-                            issue_number: commentID,
-                            body: commentBody,
-                        });
-                        core.info('Adding scan results as comment to PR #'+commentID)
-                    } catch (error:any) {
-                        core.info(error);
-                    }
+        //                 const { data: comment } = await octokit.rest.issues.createComment({
+        //                     owner: repo[0],
+        //                     repo: repo[1],
+        //                     issue_number: commentID,
+        //                     body: commentBody,
+        //                 });
+        //                 core.info('Adding scan results as comment to PR #'+commentID)
+        //             } catch (error:any) {
+        //                 core.info(error);
+        //             }
 
-                }
-                // if scan was set to fail the pipeline should fail and show a summary of the scan results
-                if ( code != null && code > 0 && options.breakBuildOnPolicyFindings){
-                    let summary_info = "Veraocde SCA Scan failed with exit code "+code+"\n"+output
-                    core.setFailed(summary_info)
-                }
-                //run(options,core.info);
-                core.info('Finish command');
-            });
-        }
+        //         }
+        //         // if scan was set to fail the pipeline should fail and show a summary of the scan results
+        //         if ( code != null && code > 0 && options.breakBuildOnPolicyFindings){
+        //             let summary_info = "Veraocde SCA Scan failed with exit code "+code+"\n"
+        //             core.setFailed(summary_info)
+        //         }
+        //         //run(options,core.info);
+        //         core.info('Finish command');
+        //     });
+        // }
         
     } catch (error) {
         if (error instanceof Error) {
+        console.log("eroor",error)
             core.info('Running scan failed.')
             //const output = stdout.toString();
             core.info(error.message);
